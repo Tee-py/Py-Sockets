65d1ab994aae261cf75fdeb6ec045ef143ed2508
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-11-04 22:57:09 +0100

    done with server

--- server.py
+++ server.py
@@ -29,6 +29,7 @@ def handle_client(conn, addr):
 
 def start():
     server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
